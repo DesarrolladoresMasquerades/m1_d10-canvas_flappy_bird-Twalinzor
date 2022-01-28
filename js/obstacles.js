3f888b143a0b43b114b5b4140e1054641112cda6
Stan Tapchylka <stanislautapchylka@gmail.com>
2022-01-28 15:38:22 +0100

    .

---
+++ js/obstacles.js
@@ -0,0 +1,15 @@
+class Obstacles {
+    constructor(ctx) {
+      this.ctx = ctx;
+      this.objects = []
+    }
+  
+    move(frameNumber) {
+      console.log("Obstacles move to frame number: ", frameNumber);
+    }
+  
+    draw(frameNumber) {
+      console.log("Obstacles draw to frame number: ", frameNumber);
+    }
+  }
+  
